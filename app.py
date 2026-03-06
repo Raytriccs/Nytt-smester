34a29a099d3692bc8e21325ed7e3a46be1e86fcd
Raytriccs <Rayaanxdutt@gmail.com>
2026-03-06 12:04:47 +0100

    la tik kommentarer og en input validering

--- app.py
+++ app.py
@@ -6,7 +6,9 @@ init_db()
 
 @app.route("/", methods=["GET", "POST"])
 def home():
+    #request.method Sjekker hvilken type request som ble sendt.
     if request.method == "POST":
+        # request form Henter data brukeren skriver i skjemaet
         navn = request.form["navn"]
         epost = request.form["epost"]
         telefon = request.form["telefon"]
@@ -14,9 +16,21 @@ def home():
         erfaring = request.form["erfaring"]
         ferdigheter = request.form["ferdigheter"]
 
-    
+        # INPUT VALIDERING
+        if not navn or not epost:
+            error = "Navn og e-post må fylles ut"
+            #render_template Viser HTML-filen.
+            return render_template("index.html", error=error)
+        # error er Variabel jeg lagde for å lagre feilmeldinger
+
+        if len(telefon) < 8:
+            error = "Telefonnummer må være minst 8 tall"
+            return render_template("index.html", error=error)
+
+        # Lagre CV i databasen
         save_cv(navn, epost, telefon, utdanning, erfaring, ferdigheter)
 
+        # Vis CV på nettsiden
         return render_template(
             "index.html",
             navn=navn,
